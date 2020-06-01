bec5f6d801c3b594327c2294f40f8ea39ceb0111
Anubhav <anubhavdhingra95@gmail.com>
2020-06-01 11:12:55 +0530

    code till before Firebase

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,20 +4,20 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 import classes from './Burger.module.css';
 
 const funcName = (props) => {
-    console.log(props, 'This is PropsObject ------------------- 1');
-    console.log(Object.keys(props.ingredients),'Converted object to array; Object.keys---------------2'); 
+    ////console.log(props, 'This is PropsObject ------------------- 1');
+    //console.log(Object.keys(props.ingredients),'Converted object to array; Object.keys---------------2'); 
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
-            console.log(igKey, 'map key -------------------3');
-            console.log(props.ingredients[igKey], 'Spread Operator------------------4');
+            //console.log(igKey, 'map key -------------------3');
+            //console.log(props.ingredients[igKey], 'Spread Operator------------------4');
             return [...Array(props.ingredients[igKey])].map((_,i) => {
-                console.log(i,'<---------VALUE   ',igKey + i, '--------------------5');
+                //console.log(i,'<---------VALUE   ',igKey + i, '--------------------5');
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             })
         }).reduce((arr, el) => {
             return arr.concat(el)
         }, []);
-        console.log(transformedIngredients);
+        //console.log(transformedIngredients);
         if (transformedIngredients.length === 0) {
             transformedIngredients = <p>Please start adding ingredients!</p>
         }
